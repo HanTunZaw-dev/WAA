d1ce1d2439dd9aa2007835cf3b1e5cefacbf52c2
Han Tun <45828855+HanTun97@users.noreply.github.com>
2023-06-16 13:18:31 -0500

    lab9

--- lab9/src/index.js
+++ lab9/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import axios from 'axios';
+
+
+axios.defaults.baseURL = 'http://localhost:8080/api/v1/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
